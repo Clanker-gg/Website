cc5a40d75cdddffc0839dcdb81305469e0218f18
mosesyu1028 <mosesyu1028@gmail.com>
2026-01-23 17:24:26 +0000

    Fix docker setup

--- backend/videofinder/settings.py
+++ backend/videofinder/settings.py
@@ -26,6 +26,10 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "http://localhost",
+    "http://127.0.0.1",
+    "https://localhost",
+    "https://127.0.0.1",
 ]
 
 ROOT_URLCONF = "videofinder.urls"
